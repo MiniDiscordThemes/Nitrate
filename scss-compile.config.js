4bab1f2546f45ab2a1409f25c1ac1316cfbc9e17
Saltssaumure <themes@saltssaumure.net>
2023-10-18 15:47:17 +0100

    Various chores

--- scss-compile.config.js
+++ scss-compile.config.js
@@ -1,13 +1,9 @@
 /** @type {import('bd-scss/lib/config').Config} */
 export default {
     meta: {
-        /** The official name of your theme. */
-        name: "Nitrate",
-        /** The name of your base scss file. */
-        scss: "nitro",
-        /** The name of your theme repository. */
-        repo: "Nitrate",
-        /** The version of your theme. */
-        version: "1.0.0"
+        name: "Nitrate" /* The official name of your theme. */,
+        scss: "nitro" /* The name of your base scss file. */,
+        repo: "Nitrate" /* The name of your theme repository. */,
+        version: "dev" /* The version of your theme. */
     }
 };
